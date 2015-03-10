62a9f49b00b7840eda46703a555e6a79d16ca23f
Rich Shea <richcshea@gmail.com>
2015-03-10 13:22:13 -0600

    user can visit homepage

---
+++ spec/features/unauthenticated_user/unauthenticated_user_can_visit_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "an unauthenticated user" do
+
+  it "can visit the home page" do
+    visit '/'
+
+    expect(page).to have_text("Welcome to Mister Pizza!")
+  end
+end
