06fd0fcb421fd84c85a7282ff2c84d6044266a1c
Rich Shea <richcshea@gmail.com>
2015-03-10 16:00:22 -0600

    add presence validations for item, category, and user

---
+++ spec/features/unauthenticated_user/unauthenticated_user_can_visit_category_pages_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "an unauthenticated user" do
+
+  it "can view category pages" do
+    user = User.create!(user_attributes)
+    category = Category.create!(name: "Pizza")
+    category.items.create!(name: "Pepperoni Pizza", price: "1000", description: "this is a pepperoni pizza", image_file_name: "image")
+    category.items.create!(name: "Cheese Pizza", price: "1100", description: "this is a cheese pizza", image_file_name: "image")
+    category.items.create!(name: "Hawaiian Pizza", price: "1200", description: "this is a hawaiian pizza", image_file_name: "image")
+    category.items.create!(name: "Vegetarian Pizza", price: "1300", description: "this is a vegetarian pizza", image_file_name: "image")
+    category.items.create!(name: "Everything Pizza", price: "1400", description: "this is an everything pizza", image_file_name: "image")
+
+    visit "/"
+    click_on_link_or_button("Pizza")
+
+    expect(current_path).to eq(category_path(category))
+    expect(page).to have_text("Pepperoni Pizza")
+    expect(page).to have_text("Everything Pizza")
+    
+    expect(page).to have_text("this is a pepperoni pizza")
+    expect(page).to have_text("this is an everything pizza")
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Item, :type => :model do
+
+  context "validations" do
+
+    it "has a name, price, description, and image_file_name" do
+      item = Item.new(item_attributes)
+
+      expect(item.valid?).to eq(true)
+    end
+
+    it "is invalid without a name" do
+      item = Item.new(item_attributes(name: nil))
+
+      expect(item.valid?).to eq(false)
+    end
+
+    it "is invalid without a price" do
+      item = Item.new(item_attributes(price: nil))
+
+      expect(item.valid?).to eq(false)
+    end
+
+    it "is invalid without a description" do
+      item = Item.new(item_attributes(description: nil))
+
+      expect(item.valid?).to eq(false)
+    end
+
+    it "is invalid without a image" do
+      item = Item.new(item_attributes(image_file_name: nil))
+
+      expect(item.valid?).to eq(false)
+    end
+  end
+
+  context "belongs to category" do
+
+    it "belongs to a category" do
+      category = Category.create!(name: "Parent")
+      category.items.new(item_attributes)
+
+      expect(category.items.first.name).to eq("itemname")
+    end
+  end
+end
