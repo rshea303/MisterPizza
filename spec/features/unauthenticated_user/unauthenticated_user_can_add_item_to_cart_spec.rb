e506b46fb48fcd5986c25d13b8191151719b75c1
Rich Shea <richcshea@gmail.com>
2015-03-11 16:34:28 -0600

    add test for persisting cart data

--- spec/features/unauthenticated_user/unauthenticated_user_can_add_item_to_cart_spec.rb
+++ spec/features/unauthenticated_user/unauthenticated_user_can_add_item_to_cart_spec.rb
@@ -31,4 +31,33 @@ describe "an unauthenticated user" do
     expect(page).to have_text("Items in Cart: 2")
 
   end
+
+  it "can login and see stored cart" do
+    user1 = User.create!(user_attributes)
+    user2 = User.create!(user_attributes(email: "user2@example.com"))
+    category = Category.create!(name: "Pizza")
+    category.items.create!(name: "Cheese Pizza",
+                           price: "50000",
+                           description: "really good",
+                           image_file_name: "default image"
+                          )
+    visit "/"
+    click_link_or_button("Log In")
+    fill_in "session[email]", with: user2.email
+    fill_in "session[password]", with: user2.password
+    click_link_or_button("Submit")
+    click_link_or_button("Pizza")
+    2.times do
+      click_link_or_button("Add To Cart")
+    end
+    expect(page).to have_text("Items in Cart: 2")
+    click_link_or_button("Log Out")
+
+    click_link_or_button("Log In")
+    fill_in "session[email]", with: user1.email
+    fill_in "session[password]", with: user1.password
+    click_link_or_button("Submit")
+
+    expect(page).to have_text("Items in Cart: 0")
+  end
 end
