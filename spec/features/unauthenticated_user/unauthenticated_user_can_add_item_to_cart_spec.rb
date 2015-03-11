a295092f7be9c6f454e03e947a7fb467f6929e4e
Rich Shea <richcshea@gmail.com>
2015-03-11 15:48:50 -0600

    visit can add multiple items to cart

--- spec/features/unauthenticated_user/unauthenticated_user_can_add_item_to_cart_spec.rb
+++ spec/features/unauthenticated_user/unauthenticated_user_can_add_item_to_cart_spec.rb
@@ -16,4 +16,19 @@ describe "an unauthenticated user" do
     expect(page).to have_text("Items in Cart: 1")
   end
 
+  it "can add multiple items to cart" do
+    category = Category.create!(name: "Pizza")
+    category.items.create!(name: "Pizza",
+                           price: "50000",
+                           description: "really good",
+                           image_file_name: "default image"
+                          )
+    visit "/"
+    click_link_or_button("Pizza")
+    2.times do
+      click_link_or_button("Add To Cart")
+    end
+    expect(page).to have_text("Items in Cart: 2")
+
+  end
 end
