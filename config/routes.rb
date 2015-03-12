42eb125bf5367d24dae7a7b5f85b74af9f428bb0
Rich Shea <richcshea@gmail.com>
2015-03-11 19:15:58 -0600

    visitor can register and log in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :items
   resource :session, only: [:new, :create, :destroy]
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
