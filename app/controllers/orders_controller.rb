74def38b15bdc469a1bb88dde3003cf569267b33
Rich Shea <richcshea@gmail.com>
2015-03-12 12:03:34 -0600

    id nil in checkout path

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
   def index
     flash[:notice] = "Thank you for your order!"
+    @user = User.find(params[:user_id])
     @orders = Order.all
     @categories = Category.all
+  end
+
+  def create
+    redirect_to user_orders_path(current_user.id)
   end
 end
--- spec/features/checking_out/authenticated_user_can_checkout_spec.rb
+++ spec/features/checking_out/authenticated_user_can_checkout_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 describe "checkout" do
 
   before do
-    category = Category.create(name: "pizza")
-    category.items.create!(name: "cheese pizza", description: "yummy", price: 2500, image_file_name: "default")
+    @category = Category.create(name: "pizza")
+    @category.items.create!(name: "cheese pizza", description: "yummy", price: 2500, image_file_name: "default")
   end
 
   context "an authenticated user" do
 
     before do
-      user = User.create!(user_attributes)
+      @user = User.create!(user_attributes)
       visit "/"
       click_link_or_button("Log In")
-      fill_in "session[email]", with: user.email
-      fill_in "session[password]", with: user.password
+      fill_in "session[email]", with: @user.email
+      fill_in "session[password]", with: @user.password
       click_link_or_button("Submit")
       click_link_or_button("pizza")
       5.times do
@@ -26,8 +26,8 @@ describe "checkout" do
     it "can checkout" do
       click_link_or_button("Checkout")
       expect(page).to have_text("Thank you for your order!")
-      expect(current_path).to eq(user_orders_path)
-      expect(page).to have_text("Order history for: #{user.email}")
+      expect(current_path).to eq(user_orders_path(@user))
+      expect(page).to have_text("Order history for: #{@user.email}")
     end
   end
 end
