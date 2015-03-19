11b543fb06bb2562ab40a9ca43d0453e1ccbd7fa
Rich Shea <richcshea@gmail.com>
2015-03-19 12:55:56 -0600

    tests passing

--- spec/features/authorizations/user_cannot_view_other_user_pages_spec.rb
+++ spec/features/authorizations/user_cannot_view_other_user_pages_spec.rb
@@ -9,16 +9,18 @@ describe "users" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     
-    @cart = Cart.new({"#{item.id}" => 10})
-    allow_any_instance_of(ApplicationController).to receive(:cart).and_return(@cart)
-    #visit "/"
+    #@cart = Cart.new({"#{item.id}" => 10})
+    #allow_any_instance_of(ApplicationController).to receive(:cart).and_return(@cart)
     
-    # how do I stub this out???
-    #click_link_or_button('pizza')
-    #10.times do
-    #  click_link_or_button('Add To Cart')
-    #end
-    #click_link_or_button("Items in Cart")
+    #  how do I stub this out???
+     visit "/"
+     click_link_or_button('pizza')
+
+     10.times do
+       click_link_or_button('Add To Cart')
+     end
+
+     click_link_or_button("Items in Cart")
     visit cart_items_path
     click_link_or_button("Checkout")
   end
