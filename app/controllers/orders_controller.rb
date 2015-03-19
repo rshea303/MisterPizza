9ed0cab735aa3d9ebe2619ad8b425910540b625f
Rich Shea <richcshea@gmail.com>
2015-03-18 20:31:53 -0600

    correct messages show for checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,15 +13,14 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(user_id: params[:user_id])
-    new_line_items = @order.create_line_items(session[:cart])
-    @order.line_items << new_line_items
-    @order.save
-    @cart.data.clear
-    redirect_to user_orders_path(params[:user_id])
+      @order = Order.new(user_id: params[:user_id])
+      new_line_items = @order.create_line_items(session[:cart])
+      @order.line_items << new_line_items
+      @order.save
+      @cart.data.clear
+      redirect_to user_orders_path(params[:user_id])
   end
  
-#private
 private
   def authorize
     unless current_user.id == params[:user_id].to_i
